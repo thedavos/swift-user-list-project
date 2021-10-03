89f0e8ed2e762862a102a1ad9fa7318903172abb
David <davidvargas.d45@gmail.com>
2021-10-02 21:28:12 -0500

    Initial Commit

---
+++ cibertec_examen_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cibertec_examen_1
+//
+//  Created by Global66 on 2/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
